86c90605221f3145073944bb74aa19f204fd00fc
danielstrolle <danielstrolle@gmail.com>
2017-08-28 14:57:11 -0500

    Add functions to reduce duplicate code using 'this', complete percent and plus/minus buttons

--- js/calculator.js
+++ js/calculator.js
@@ -4,104 +4,134 @@ var listener = function (click) {
     alert("you pressed the button");
 }
 //Start of functions to handle button input into operand one
-var inputOne = function (click) {
-//    add value of id=one to value of form operandone
+var inputNumber= function (click) {
     if (document.getElementById('operator').value === ""){
-    var valueToAdd= document.getElementById('one').value;
-    document.getElementById('operand-one').value+=valueToAdd;
-} else {
-        var valuesToAddTwo=document.getElementById('one').value;
-        document.getElementById('operand-two').value+=valuesToAddTwo;
-    }
-}
-var inputTwo = function (click) {
-//    add value of id=one to value of form operandone
-    if (document.getElementById('operator').value === ""){
-    var valueToAdd= document.getElementById('two').value;
-    document.getElementById('operand-one').value+=valueToAdd;
-}else{
-        var valuesToAddTwo=document.getElementById('two').value;
-        document.getElementById('operand-two').value+=valuesToAddTwo;
-    }
-}
-var inputThree = function (click) {
-//    add value of id=one to value of form operandone
-    if (document.getElementById('operator').value === ""){
-        var valueToAdd= document.getElementById('three').value;
-        document.getElementById('operand-one').value+=valueToAdd;
+        document.getElementById('operand-one').value+=this.value;
     } else {
-        var valuesToAddTwo=document.getElementById('three').value;
+        document.getElementById('operand-two').value+=this.value
+    }
+}
+// var inputOne = function (click) {
+// //    add value of id=one to value of form operandone
+//     if (document.getElementById('operator').value === ""){
+//     var valueToAdd= document.getElementById('one').value;
+//     document.getElementById('operand-one').value+=valueToAdd;
+// } else {
+//         var valuesToAddTwo=document.getElementById('one').value;
+//         document.getElementById('operand-two').value+=valuesToAddTwo;
+//     }
+// }
+// var inputTwo = function (click) {
+// //    add value of id=one to value of form operandone
+//     if (document.getElementById('operator').value === ""){
+//     var valueToAdd= document.getElementById('two').value;
+//     document.getElementById('operand-one').value+=valueToAdd;
+// }else{
+//         var valuesToAddTwo=document.getElementById('two').value;
+//         document.getElementById('operand-two').value+=valuesToAddTwo;
+//     }
+// }
+// var inputThree = function (click) {
+// //    add value of id=one to value of form operandone
+//     if (document.getElementById('operator').value === ""){
+//         var valueToAdd= document.getElementById('three').value;
+//         document.getElementById('operand-one').value+=valueToAdd;
+//     } else {
+//         var valuesToAddTwo=document.getElementById('three').value;
+//         document.getElementById('operand-two').value+=valuesToAddTwo;
+//     }
+// }
+// var inputFour = function (click) {
+// //    add value of id=one to value of form operandone
+//     if (document.getElementById('operator').value === ""){
+//         var valueToAdd= document.getElementById('four').value;
+//         document.getElementById('operand-one').value+=valueToAdd;
+//     } else {
+//         var valuesToAddTwo=document.getElementById('four').value;
+//         document.getElementById('operand-two').value+=valuesToAddTwo;
+//     }
+// }
+// var inputFive = function (click) {
+// //    add value of id=one to value of form operandone
+//     if (document.getElementById('operator').value === ""){
+//         var valueToAdd= document.getElementById('five').value;
+//         document.getElementById('operand-one').value+=valueToAdd;
+//     } else {
+//         var valuesToAddTwo=document.getElementById('five').value;
+//         document.getElementById('operand-two').value+=valuesToAddTwo;
+//     }
+// }
+// var inputSix = function (click) {
+// //    add value of id=one to value of form operandone
+//     if (document.getElementById('operator').value === ""){
+//         var valueToAdd= document.getElementById('six').value;
+//         document.getElementById('operand-one').value+=valueToAdd;
+//     } else {
+//         var valuesToAddTwo=document.getElementById('six').value;
+//         document.getElementById('operand-two').value+=valuesToAddTwo;
+//     }
+// }
+// var inputSeven = function (click) {
+// //    add value of id=one to value of form operandone
+//     if (document.getElementById('operator').value === ""){
+//         var valueToAdd= document.getElementById('seven').value;
+//         document.getElementById('operand-one').value+=valueToAdd;
+//     } else {
+//         var valuesToAddTwo=document.getElementById('seven').value;
+//         document.getElementById('operand-two').value+=valuesToAddTwo;
+//     }
+// }
+// var inputEight = function (click) {
+// //    add value of id=one to value of form operandone
+//     if (document.getElementById('operator').value === ""){
+//         var valueToAdd= document.getElementById('eight').value;
+//         document.getElementById('operand-one').value+=valueToAdd;
+//     } else {
+//         var valuesToAddTwo=document.getElementById('eight').value;
+//         document.getElementById('operand-two').value+=valuesToAddTwo;
+//     }
+// }
+// var inputNine = function (click) {
+// //    add value of id=one to value of form operandone
+//     if (document.getElementById('operator').value === ""){
+//         var valueToAdd= document.getElementById('nine').value;
+//         document.getElementById('operand-one').value+=valueToAdd;
+//     } else {
+//         var valuesToAddTwo=document.getElementById('nine').value;
+//         document.getElementById('operand-two').value+=valuesToAddTwo;
+//     }
+// }
+// var inputZero = function (click) {
+// //    add value of id=one to value of form operandone
+//     if (document.getElementById('operator').value === ""){
+//         var valueToAdd= document.getElementById('zero').value;
+//         document.getElementById('operand-one').value+=valueToAdd;
+//     } else {
+//         var valuesToAddTwo=document.getElementById('zero').value;
+//         document.getElementById('operand-two').value+=valuesToAddTwo;
+//     }
+// }
+var inputDecimal = function (click) {
+    if (document.getElementById('operator').value ===""){
+        var valueToAdd= document.getElementById('decimal').value;
+        document.getElementById('operand-one').value+=valueToAdd
+    } else {
+        var valuesToAddTwo=document.getElementById('decimal').value;
         document.getElementById('operand-two').value+=valuesToAddTwo;
     }
 }
-var inputFour = function (click) {
-//    add value of id=one to value of form operandone
-    if (document.getElementById('operator').value === ""){
-        var valueToAdd= document.getElementById('four').value;
-        document.getElementById('operand-one').value+=valueToAdd;
+var inputPlusMinus = function (click) {
+    if (document.getElementById('operator').value ===""){
+        document.getElementById('operand-one').value*=-1
     } else {
-        var valuesToAddTwo=document.getElementById('four').value;
-        document.getElementById('operand-two').value+=valuesToAddTwo;
+        document.getElementById('operand-two').value*=-1
     }
 }
-var inputFive = function (click) {
-//    add value of id=one to value of form operandone
-    if (document.getElementById('operator').value === ""){
-        var valueToAdd= document.getElementById('five').value;
-        document.getElementById('operand-one').value+=valueToAdd;
+var inputPercent = function (click) {
+    if (document.getElementById('operator').value ===""){
+        document.getElementById('operand-one').value*=.01
     } else {
-        var valuesToAddTwo=document.getElementById('five').value;
-        document.getElementById('operand-two').value+=valuesToAddTwo;
-    }
-}
-var inputSix = function (click) {
-//    add value of id=one to value of form operandone
-    if (document.getElementById('operator').value === ""){
-        var valueToAdd= document.getElementById('six').value;
-        document.getElementById('operand-one').value+=valueToAdd;
-    } else {
-        var valuesToAddTwo=document.getElementById('six').value;
-        document.getElementById('operand-two').value+=valuesToAddTwo;
-    }
-}
-var inputSeven = function (click) {
-//    add value of id=one to value of form operandone
-    if (document.getElementById('operator').value === ""){
-        var valueToAdd= document.getElementById('seven').value;
-        document.getElementById('operand-one').value+=valueToAdd;
-    } else {
-        var valuesToAddTwo=document.getElementById('seven').value;
-        document.getElementById('operand-two').value+=valuesToAddTwo;
-    }
-}
-var inputEight = function (click) {
-//    add value of id=one to value of form operandone
-    if (document.getElementById('operator').value === ""){
-        var valueToAdd= document.getElementById('eight').value;
-        document.getElementById('operand-one').value+=valueToAdd;
-    } else {
-        var valuesToAddTwo=document.getElementById('eight').value;
-        document.getElementById('operand-two').value+=valuesToAddTwo;
-    }
-}
-var inputNine = function (click) {
-//    add value of id=one to value of form operandone
-    if (document.getElementById('operator').value === ""){
-        var valueToAdd= document.getElementById('nine').value;
-        document.getElementById('operand-one').value+=valueToAdd;
-    } else {
-        var valuesToAddTwo=document.getElementById('nine').value;
-        document.getElementById('operand-two').value+=valuesToAddTwo;
-    }
-}
-var inputZero = function (click) {
-//    add value of id=one to value of form operandone
-    if (document.getElementById('operator').value === ""){
-        var valueToAdd= document.getElementById('zero').value;
-        document.getElementById('operand-one').value+=valueToAdd;
-    } else {
-        var valuesToAddTwo=document.getElementById('zero').value;
-        document.getElementById('operand-two').value+=valuesToAddTwo;
+        document.getElementById('operand-two').value*=.01;
     }
 }
 //End of functions to handle button input into operand one
@@ -150,7 +180,6 @@ var equality= function(click){
 
 //    clear all the forms in the calculator
 //     document.getElementById('operand-one').value="";
-    document.getElementById('operator').value="";
     document.getElementById('operand-two').value="";
 //    return calculated value to operand one
     document.getElementById('operand-one').value=calculated;
@@ -163,19 +192,20 @@ var clear= function(click){
     document.getElementById('operand-two').value="";
 }
 //Loggers from various functions above
-document.getElementById("one").addEventListener('click', inputOne, false);
-document.getElementById("two").addEventListener('click', inputTwo, false);
-document.getElementById("three").addEventListener('click', inputThree, false);
-document.getElementById("four").addEventListener('click', inputFour, false);
-document.getElementById("five").addEventListener('click', inputFive, false);
-document.getElementById("six").addEventListener('click', inputSix, false);
-document.getElementById("seven").addEventListener('click', inputSeven, false);
-document.getElementById("eight").addEventListener('click', inputEight, false);
-document.getElementById("nine").addEventListener('click', inputNine, false);
-document.getElementById("zero").addEventListener('click', inputZero, false);
+
+var numberInputs = document.getElementsByClassName('number-input');
+
+for (var i = 0; i < numberInputs.length; i++){
+    numberInputs[i].addEventListener('click', inputNumber, false);
+}
+
+document.getElementById("decimal").addEventListener('click', inputDecimal, false);
 document.getElementById("plus").addEventListener('click', operatorPlus, false);
 document.getElementById("minus").addEventListener('click', operatorMinus, false);
 document.getElementById("multiply").addEventListener('click', operatorMultiply, false);
 document.getElementById("divide").addEventListener('click', operatorDivide, false);
 document.getElementById("clear").addEventListener('click', clear, false);
 document.getElementById("equals").addEventListener('click', equality, false);
+document.getElementById("plus-minus").addEventListener('click', inputPlusMinus, false);
+document.getElementById("percent").addEventListener('click', inputPercent, false);
+
