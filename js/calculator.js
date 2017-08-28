2669d6ac32784fd59a83bb8dcc05b577ca08589e
danielstrolle <danielstrolle@gmail.com>
2017-08-28 10:49:58 -0500

    Add if else statement to functions allowing input into operand one only when the operator is an empty string

--- js/calculator.js
+++ js/calculator.js
@@ -4,24 +4,41 @@ var listener = function (click) {
     alert("you pressed the button");
 }
 //Start of functions to handle button input into operand one
-var testerOne = function (click) {
+var inputOne = function (click) {
 //    add value of id=one to value of form operandone
+    if (document.getElementById('operator').value === ""){
     var valueToAdd= document.getElementById('one').value;
     console.log(valueToAdd);
     document.getElementById('operand-one').value+=valueToAdd;
     console.log(document.getElementById('operand-one'));
+} else {
+        document.getElementById('operand-two').value+=valueToAdd;
+    }
 }
-var testerTwo = function (click) {
+var inputTwo = function (click) {
 //    add value of id=one to value of form operandone
+    if (document.getElementById('operator').value === ""){
     var valueToAdd= document.getElementById('two').value;
     console.log(valueToAdd);
     document.getElementById('operand-one').value+=valueToAdd;
     console.log(document.getElementById('operand-one'));
-}
+}}
 //End of functions to handle button input into operand one
 
+// Function for the operator form
+var operatorPlus = function (click) {
+    var operatorToUse = document.getElementById('plus').value;
+    document.getElementById('operator').value=operatorToUse;
+    console.log(document.getElementById('operator'));
+}
+var operatorMinus = function (click) {
+    var operatorToUse = document.getElementById('minus').value;
+    document.getElementById('operator').value=operatorToUse;
+    console.log(document.getElementById('operator'));
+}
+
 //Loggers from various functions above
-document.getElementById("one").addEventListener('click', testerOne, false);
-document.getElementById("two").addEventListener('click', testerTwo, false);
-document.getElementById("plus").addEventListener('click', listener, false);
-document.getElementById("equals").addEventListener('click', listener, false);
+document.getElementById("one").addEventListener('click', inputOne, false);
+document.getElementById("two").addEventListener('click', inputTwo, false);
+document.getElementById("plus").addEventListener('click', operatorPlus, false);
+document.getElementById("minus").addEventListener('click', operatorMinus, false);
